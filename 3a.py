78f2b7c67fea85854ad8093ca31fc7897a23de59
Aria <aria@campus-005-204.ucdavis.edu>
2018-02-11 18:12:43 -0800

    Implemented objective function and SGD with decaying step size

--- 3a.py
+++ 3a.py
@@ -3,6 +3,7 @@ import random
 import sys
 import numpy as np
 from random import shuffle
+from numpy import random as rd
 
 ## constants
 inputData = "mnist_2_vs_7/mnist_X_train.dat"
@@ -10,7 +11,6 @@ expectedOutputData = "mnist_2_vs_7/mnist_Y_train.dat"
 xTestData = "mnist_2_vs_7/mnist_X_test.dat"
 yTestData = "mnist_2_vs_7/mnist_Y_test.dat"
 
-learningRate = .1
 xfileSize = 10000
 
 output = []
@@ -23,42 +23,64 @@ def main():
 	yfile = np.loadtxt(expectedOutputData)
 	testX = np.loadtxt(xTestData)
 	testY = np.loadtxt(yTestData)
+	prob = 0
 
-	t = 0
-	wGD = np.zeros(len(xfile[0]))
-	iterations = 50
-	batch = len(xfile)
-	training(wGD, xfile, yfile, iterations, batch) # part 3a
-	success = predAccu(wGD, testX, testY) # part 3b
-#	print (success) # tests accuracy for 3a/b
+	# 3a/3b
+	# t = 0
+	# learningRate = .1
+	# wGD = np.zeros(len(xfile[0]))
+	# iterationsGD = 50
+	# batchGD = len(xfile)
+	# trainedWGD = training(learningRate, wGD, xfile, yfile, iterationsGD, batchGD) # part 3a
+	# success = predAccu(wGD, testX, testY) # part 3b
+	# print (success) # tests accuracy for 3a/b
+	# objFun(xfile, yfile, trainedWGD)
 
+	# 3c
+# 	learningRate = .00001
+# 	iterationsSGD = 350
+# 	batchSGD = 256 # randomly choose n=256 data points from X
+# 	np.random.shuffle(xfile) # shuffle input/training vectors/data to stochastically choose N data points
+# 	wSGD = np.zeros(len(xfile[0])) # reinitializes weight
+# 	trainedW = training(learningRate, wSGD, xfile, yfile, iterationsSGD, batchSGD, prob)
+# 	successSGD = predAccu(trainedW, testX, testY)
+# #	print(trainedW)
+# 	print (successSGD)
 
-	n = 256 # randomly choose n=256 data points from X
-#	trainedW = SGD(wSGD, n, xfile, yfile) # 3c: function to implement SGD on logistic regression
+	# #SGD with decaying step size, 3d
+	prob = "3d"
+	learningRate = .00001
+	iterationsSGD = 350
+	batchSGD = 256 # randomly choose n=256 data points from X
 	np.random.shuffle(xfile) # shuffle input/training vectors/data to stochastically choose N data points
-	wSGD = np.zeros(len(xfile[0])) # reinitializes weight
-	trainedW = trainingSGD(wSGD, xfile, yfile)
+	wDecaySGD = np.zeros(len(xfile[0])) # reinitializes weight
+	trainedW = training(learningRate, wDecaySGD, xfile, yfile, iterationsSGD, batchSGD, prob)
 	successSGD = predAccu(trainedW, testX, testY)
-#	print(trainedW)
-	print (successSGD)
-#	predAccuSGD(w, testX, testY)
+	print(successSGD)	
 
-def trainingSGD(wSGD, xfile, yfile):
-	batch = 256
-	iterations = 500
-	training(wSGD, xfile, yfile, iterations, batch) # part 3a
-	return wSGD
 
-def training(wGD, xfile, yfile, iterations, batch): 
+def objFun(xfile, yfile, w):
+	for i in range(len(xfile)):
+		comparison = 1 - yfile[i] * np.dot(np.transpose(w), xfile[i])
+		En = max(0, comparison)
+		if En != 0:
+			print(En)
+	return
+
+def training(learningRate, w, xfile, yfile, iterations, batch, prob): 
 	for i in range(iterations): # random big number
-		grad = summation(wGD, xfile, yfile, batch)/len(xfile) # compute the gradient
-		wGD += learningRate * grad
-	return wGD # updated weight
+		if batch < len(yfile):	# SGD case	
+			np.random.shuffle(xfile) # shuffle input/training vectors/data to stochastically choose N data points
+			if prob == '3d' and i % 50 == 0:
+				learningRate * .5
+		grad = summation(i, w, xfile, yfile, batch)/batch # compute the gradient
+		w += learningRate * grad
+	return w # updated weight
 
-def summation(wGD, xfile, yfile, batch): # summation in gradient
+def summation(i, w, xfile, yfile, batch): # summation in gradient
 	sum = 0.0
 	for j in range(batch): # i should go from 1
-		sum += xfile[j] * yfile[j] / (1 + np.exp(yfile[j] * np.dot(np.transpose(wGD), xfile[j])))
+		sum += xfile[j] * yfile[j] / (1 + np.exp(yfile[j] * np.dot(np.transpose(w), xfile[j])))
 	return sum
 
 def predAccu(w, testX, testY): # part 3b, sum of err divided by xfilelen
